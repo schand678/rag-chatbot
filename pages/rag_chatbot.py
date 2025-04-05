d2e82c22596819c19dfab01474c83a6ab4819513
schand678 <sahilchand055@gmail.com>
2025-04-05 00:32:34 -0600

    Update rag_chatbot.py

--- pages/rag_chatbot.py
+++ pages/rag_chatbot.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import sys
+import os
+sys.path.append(os.path.abspath(".."))
 from ragmodel import rag_chatbot
 
 st.set_page_config(page_title="RAG Chatbot", page_icon="🤖")
